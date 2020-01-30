677c989fa22e7fb942809fb16e0d9b461ecfb7ac
HJfod <dreadrollmusic@gmail.com>
2020-01-30 21:53:09 +0200

    buttons n shit

--- app.py
+++ app.py
@@ -2,26 +2,45 @@ import tkinter as tk
 from tkinter import filedialog, Text
 import os
 
+button_width = 40
+
 root = tk.Tk()
 
-#canvas = tk.Canvas(root, height=500, width=700, bg="#aa00ff")
+root.title('app')
+root.geometry('500x300')
+root.configure(bg='#000')
+
+def hover_e(e):
+	e.configure(bg='#444')
+def hover_l(e):
+	e.configure(bg='#000')
+
+#canvas = tk.Canvas(root, height=300, width=500, bg="#000000")
 #canvas.pack()
 
-def donothing():
-    x = 0
+#menubar = tk.Menu(root)
+#filemenu = tk.Menu(menubar, tearoff=0)
+#filemenu.add_command(label="New", command=donothing)
+#filemenu.add_command(label="Open", command=donothing)
+#filemenu.add_command(label="Save", command=donothing)
+#filemenu.add_separator()
+#filemenu.add_command(label="Exit", command=root.quit)
+#menubar.add_cascade(label="File", menu=filemenu)
 
-menubar = tk.Menu(root)
-filemenu = tk.Menu(menubar, tearoff=0)
-filemenu.add_command(label="New", command=donothing)
-filemenu.add_command(label="Open", command=donothing)
-filemenu.add_command(label="Save", command=donothing)
-filemenu.add_separator()
-filemenu.add_command(label="Exit", command=root.quit)
-menubar.add_cascade(label="File", menu=filemenu)
+root.update()
 
-button = tk.Button(root, text="this a button bro", font=("Comic Sans MS",20,"normal"), border=0, padx=5, pady=20, fg="#00f", bg="#00ff00")
-button.pack()
+b_exit = tk.Button(root, text='✕', font=('Roboto Lt',12,'normal'), border=0, padx=5, pady=20, fg="#fff", bg="#0a0a0a", activebackground='#f00', activeforeground='#fff', command=root.quit)
+b_exit.place(width = button_width, height=25, x = root.winfo_width()-button_width, y = 0)
+b_exit.bind('<Enter>', hover_e)
+b_exit.bind('<Leave>', hover_l)
 
-root.config(menu=menubar)
+b_fs = tk.Button(root, text='✕', font=('Roboto Lt',12,'normal'), border=0, padx=5, pady=20, fg="#fff", bg="#0a0a0a", activebackground='#f00', activeforeground='#fff', command=root.quit)
+b_fs.place(width = button_width, height=25, x = root.winfo_width()-button_width*2, y = 0)
+b_fs.bind('<Enter>', hover_e)
+b_fs.bind('<Leave>', hover_l)
+
+#root.config(menu=menubar)
+
+root.overrideredirect(1)
 root.mainloop()
 
