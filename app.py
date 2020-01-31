15153c63311c0d77038a98aa34baaa3f8d14f0ed
HJfod <dreadrollmusic@gmail.com>
2020-01-31 16:52:00 +0200

    fixed button stuff

--- app.py
+++ app.py
@@ -5,15 +5,22 @@ import os
 button_width = 40
 
 root = tk.Tk()
-
-root.title('app')
 root.geometry('500x300')
+root.title('app')
 root.configure(bg='#000')
 
 def hover_e(e):
-	e.configure(bg='#444')
+	e.widget.configure(bg='#444')
+
 def hover_l(e):
-	e.configure(bg='#000')
+	e.widget.configure(bg='#000')
+
+def fullscreen(x):
+	root.attributes('-fullscreen','%(b)d' % {'b': x})
+	root.update()
+
+def confg(e):
+	
 
 #canvas = tk.Canvas(root, height=300, width=500, bg="#000000")
 #canvas.pack()
@@ -29,15 +36,18 @@ def hover_l(e):
 
 root.update()
 
-b_exit = tk.Button(root, text='✕', font=('Roboto Lt',12,'normal'), border=0, padx=5, pady=20, fg="#fff", bg="#0a0a0a", activebackground='#f00', activeforeground='#fff', command=root.quit)
-b_exit.place(width = button_width, height=25, x = root.winfo_width()-button_width, y = 0)
-b_exit.bind('<Enter>', hover_e)
-b_exit.bind('<Leave>', hover_l)
+class app:
+	b_exit = tk.Button(root, text='✕', font=('Roboto Lt',12,'normal'), border=0, padx=5, pady=20, fg="#fff", bg="#0a0a0a", activebackground='#f00', activeforeground='#fff', command=root.quit)
+	b_exit.place(width = button_width, height=25, x = root.winfo_width()-button_width, y = 0)
+	b_exit.bind('<Enter>', hover_e)
+	b_exit.bind('<Leave>', hover_l)
+	
+	b_fs = tk.Button(root, text='✕', font=('Roboto Lt',12,'normal'), border=0, padx=5, pady=20, fg="#fff", bg="#0a0a0a", activebackground='#f00', activeforeground='#fff', command=fullscreen(1))
+	b_fs.place(width = button_width, height=25, x = root.winfo_width()-button_width*2, y = 0)
+	b_fs.bind('<Enter>', hover_e)
+	b_fs.bind('<Leave>', hover_l)
 
-b_fs = tk.Button(root, text='✕', font=('Roboto Lt',12,'normal'), border=0, padx=5, pady=20, fg="#fff", bg="#0a0a0a", activebackground='#f00', activeforeground='#fff', command=root.quit)
-b_fs.place(width = button_width, height=25, x = root.winfo_width()-button_width*2, y = 0)
-b_fs.bind('<Enter>', hover_e)
-b_fs.bind('<Leave>', hover_l)
+root.bind('<Configure>', confg)
 
 #root.config(menu=menubar)
 
