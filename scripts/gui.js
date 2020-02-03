6b274281aac8dd29886bfc47d9bfee769f14c445
hjfod <dreadrollmusic@gmail.com>
2020-02-03 19:23:12 +0200

    fixed contextmenu

--- scripts/gui.js
+++ scripts/gui.js
@@ -36,44 +36,46 @@ ipc.on('app', (event, arg) => {
 
 $(document).click( () => {
 	close_menu();
+}).keypress( () => {
+	close_menu()
 });
 
 $('[data-menu]').contextmenu( (e) => {
 	let m = $('#menu_window');
 	let t = m.is(':hidden');
 	
-	if (t){
-		let v = $(e.target).attr('data-menu');
-		v = v.split('//');
-		
-		for (let i = 0; i < v.length; i++){
-			if (v[i] === '---'){
-				let n_s = document.createElement('text');
-				n_s.innerHTML = '───────────';
-				n_s.setAttribute('class','menu_separator');
-				m.append(n_s);
-			}else{
-				v[i] = v[i].split('\\');
-				let n_o = document.createElement('button');
-				n_o.innerHTML = v[i][0];
-				n_o.setAttribute('class','menu_option');
-				n_o.setAttribute('onclick',v[i][2]);
-				m.append(n_o);
-			}
+	let v = $(e.target).attr('data-menu');
+	v = v.split('//');
+	
+	for (let i = 0; i < v.length; i++){
+		if (v[i] === '---'){
+			let n_s = document.createElement('text');
+			n_s.innerHTML = '───────────';
+			n_s.setAttribute('class','menu_separator');
+			m.append(n_s);
+		}else{
+			v[i] = v[i].split('\\');
+			let n_o = document.createElement('button');
+			n_o.innerHTML = v[i][0];
+			n_o.setAttribute('class','menu_option');
+			n_o.setAttribute('onclick',v[i][2]);
+			m.append(n_o);
 		}
-		
-		let mex = mouse_x, mey = mouse_y;
-		if (mouse_y > window.innerHeight - m.css('height')){
-			mey = mouse_y - m.css('height');
-		}
-		m.css('left',mex + 'px').css('top',mey + 'px');
-		
-		m.show();
-	}else{
-		close_menu();
-	};
+	}
+	
+	let mex = mouse_x, mey = mouse_y;
+	if (mouse_y > window.innerHeight - m.css('height')){
+		mey = mouse_y - m.css('height');
+	}
+	m.css('left',mex + 'px').css('top',mey + 'px');
+	
+	m.show();
 });
 
 function close_menu() {
-	$('#menu_window').hide().empty();
+	let t = $('#menu_select').is(':hidden'), t2 = $('#menu_window').is(':hover');
+	if (t || !t2){
+		$('#menu_window').hide().empty();
+		$('#menu_select').hide().empty();
+	}
 }
